f519806177847c9eab7c39bb56fe290b97ad675b
Dragos2099 <dragos.tudosoiu@student.upt.ro>
2020-03-27 00:39:02 +0200

    SM-4 Added styling for the view-students scene

--- src/loose/oose/fis/lab/student/manager/controllers/LoginController.java
+++ src/loose/oose/fis/lab/student/manager/controllers/LoginController.java
@@ -1,9 +1,15 @@
 package loose.oose.fis.lab.student.manager.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
 
